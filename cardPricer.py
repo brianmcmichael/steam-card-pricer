af0083fed974850ca3bcf4326c0c42d8f634b652
Ian Harmon <ianmharmon@gmail.com>
2013-07-30 17:36:34 -0400

    automatic update

--- cardPricer.py
+++ cardPricer.py
@@ -285,7 +285,7 @@ def upload():
 ################################################################################
 # Program entrypoint.
 if __name__ == "__main__":
-  updateData()
+  #updateData()
   updateCounts()
   updateSite()
   upload()
